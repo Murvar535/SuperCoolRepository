f252cbc481d480d2fd3dcc72d9195d6042588c91
Murvar535 <142189055+Murvar535@users.noreply.github.com>
2025-12-14 20:26:58 +0900

    Add files via upload

---
+++ JavaCapsule/BankAccount.java
@@ -0,0 +1,19 @@
+public class BankAccount {
+    private double balance;  // Закрытое поле
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
